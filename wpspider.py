b3f2e5a857d3a782db2ce182fbaf63922e20a76c
magicdu <2760522291@qq.com>
2017-02-27 17:51:22 +0800

    add get_urls(..)

--- wpspider.py
+++ wpspider.py
@@ -6,6 +6,7 @@ from wordpress_xmlrpc import Client,WordPressPost
 from wordpress_xmlrpc.methods.posts import GetPosts,NewPost
 from wordpress_xmlrpc.methods.users import GetUserInfo
 import re
+import time
 
 
 #新闻类
@@ -16,13 +17,29 @@ class News(object):
 		self.category=category #分类
 		self.content=content   #内容
 
+#获取最新的新闻链接列表
+
+'''
+url  :需要抓取的网址
+n    :获取链接的数量,即每次需要发布新文章的数量
+links:返回链接列表
+'''
+def get_urls(url,n=1):
+	links=[]
+	html=urlopen(url)
+	bsObj=BeautifulSoup(html,'html.parser')
+	for link in bsObj.find('div',{'class':'main2_left_fir_left'}).findAll('a')[0:n]:
+		if 'href' in link.attrs:
+			links.append(link.attrs['href'])
+	return links
+
 
 #根据文章链接切分文章
 '''
-这里是以金融之家为例的,抓取其他资讯请自行分析网站文章重写 get_news(url)
+这里是以金融之家为例的,抓取其他资讯请自行分析网站文章重写 get_news(link)
 '''
-def get_news(url):
-	html=urlopen(url)
+def get_news(link):
+	html=urlopen(link)
 	bsObj=BeautifulSoup(html,'html.parser')
 	title=bsObj.h1.get_text()
 	#print('标题:',title)
@@ -33,15 +50,15 @@ def get_news(url):
 	content=bsObj.find('div',{'class':'news_content'}).prettify()
 	#print('内容:',content)
 	news=News(title,tags,category,content)
-    return news
+	return news
 
 #发送新闻到wordpress
 
 '''
 yourwebsit: 你的wordpress地址+xmlrpc.php
-username:wordpress登录用户名
-password:wordpress登录密码
-news:新闻对象
+username  : wordpress登录用户名
+password  : wordpress登录密码
+news      : 新闻对象
 '''
 
 def send_news(yourwebsit,username,password,news):
@@ -57,7 +74,14 @@ def send_news(yourwebsit,username,password,news):
 	wp.call(NewPost(post))
 
 
-#函数调用示例：
-news=get_news('http://www.jrzj.com/180636.html')
-send_news('http://www.baidu.com/xmlrpc.php','abc','123',news)
+
+
+
+#以金融之家为例
+l=get_urls('http://www.jrzj.com',3)
+for link in l:
+	news=get_news(link)
+	#print(news.title) #打印文章标题
+	time.sleep(5)
+	send_news('http://blog.abc.com/xmlrpc.php','username','password',news)
 
